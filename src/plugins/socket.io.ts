4973a97923d608291f770a3f58f9d2c8c9a7ec2d
NaMax66 <max.naid@mail.ru>
2022-10-24 12:18:49 +0600

    configure vite for .env

--- src/plugins/socket.io.ts
+++ src/plugins/socket.io.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 export const useSocketIO = () => {
   // @ts-ignore
-  const socket = io(import.meta.env.VITE_API_URI)
+  const socket = io(import.meta.env.APP_API_URI)
   return {
     socket,
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+const ENV_PREFIX = 'APP_'
+
+// @ts-ignore
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ENV_PREFIX)
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    preview: {
+      /* @ts-ignore */
+      host: env.APP_IP,
+      /* @ts-ignore */
+      port: env.APP_PORT,
+    },
+
+    envPrefix: ENV_PREFIX
+  });
+}
